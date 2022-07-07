4419d75a9e70d347c2a1a1417c6a9b7eb5d544b9
Maximilian Albert <maximilian.albert@gmail.com>
2022-07-07 22:45:12 +0100

    Display versions of iommi, django and python on index page

--- core/views.py
+++ core/views.py
@@ -1,5 +1,23 @@
+import platform
+import textwrap
+
+import django
+import iommi
 from django.http import HttpResponse
+from django.template import Template, RequestContext
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    context = RequestContext(request)
+    context["python_version"] = platform.python_version()
+    context["django_version"] = django.__version__
+    context["iommi_version"] = iommi.__version__
+    template = Template(textwrap.dedent("""
+    <h3>Versions</h3>
+        <ul>
+            <li>Python: {{ python_version }}</li>
+            <li>Django: {{ django_version }}</li>
+            <li>Iommi: {{ iommi_version }}</li>
+        </ul>
+        """))
+    return HttpResponse(template.render(context))
