775603581b0f1af16d305129ea732b0a9d204b0d
Maximilian Albert <maximilian.albert@gmail.com>
2022-07-07 22:53:38 +0100

    Add Artist model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=255, db_index=True, verbose_name='name')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'artist'
+        verbose_name_plural = 'artists'
