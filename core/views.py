33e52ccc6947adb7f88a597e1e3e8ccf5ee32c9a
Maximilian Albert <maximilian.albert@gmail.com>
2022-07-07 23:37:30 +0100

    Refactor version info to use iommi.Page

--- core/views.py
+++ core/views.py
@@ -1,23 +1,20 @@
 import platform
-import textwrap
 
 import django
 import iommi
-from django.http import HttpResponse
-from django.template import Template, RequestContext
+from iommi import Page, html
 
 
 def index(request):
-    context = RequestContext(request)
-    context["python_version"] = platform.python_version()
-    context["django_version"] = django.__version__
-    context["iommi_version"] = iommi.__version__
-    template = Template(textwrap.dedent("""
-    <h3>Versions</h3>
-        <ul>
-            <li>Python: {{ python_version }}</li>
-            <li>Django: {{ django_version }}</li>
-            <li>Iommi: {{ iommi_version }}</li>
-        </ul>
-        """))
-    return HttpResponse(template.render(context))
+    class VersionInfo(Page):
+        class Meta:
+            title = "Versions"
+            h_tag__tag = "h3"
+
+        versions = html.ul(
+            html.li(f"Python: {platform.python_version()}"),
+            html.li(f"Django: {django.__version__}"),
+            html.li(f"Iommi: {iommi.__version__}"),
+        )
+
+    return VersionInfo()
