46cf936e54ac4341eb86aba7464189ba2508c64e
Maximilian Albert <maximilian.albert@gmail.com>
2022-07-07 23:13:06 +0100

    Add model Album

--- core/models.py
+++ core/models.py
@@ -11,3 +11,17 @@ class Artist(models.Model):
         ordering = ('name',)
         verbose_name = 'artist'
         verbose_name_plural = 'artists'
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=255, db_index=True, verbose_name='name')
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='albums', verbose_name='artist')
+    year = models.IntegerField(verbose_name='year')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'album'
+        verbose_name_plural = 'albums'
