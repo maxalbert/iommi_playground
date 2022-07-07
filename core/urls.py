e11ee16e3043926ca51aba2bed879e1c3f11b93f
Maximilian Albert <maximilian.albert@gmail.com>
2022-07-07 23:58:16 +0100

    Add sections for model pages and error pages

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
+from iommi import Table, Page, html
 
 from . import views
+from .models import Artist
+
+
+def error_page_parts_must_be_dict_not_list(request):
+    return Page(parts=[html.div("Error")])
+
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
+    path("artists/", Table(auto__model=Artist).as_view(), name="artists"),
+    path(
+        "errors/page_parts_must_be_dict_not_list",
+        error_page_parts_must_be_dict_not_list,
+        name="error_page_parts_must_be_dict_not_list",
+    ),
 ]
--- core/views.py
+++ core/views.py
@@ -2,19 +2,53 @@ import platform
 
 import django
 import iommi
+from django.urls import reverse_lazy
 from iommi import Page, html
 
 
+class MyPage(Page):
+    class Meta:
+        h_tag__tag = "h3"
+
+
+def hyperlink(link_text, url_name):
+    return html.li(html.a(link_text, attrs__href=reverse_lazy(url_name)))
+
+
+class VersionInfo(MyPage):
+    class Meta:
+        title = "Versions"
+
+    versions = html.ul(
+        html.li(f"Python: {platform.python_version()}"),
+        html.li(f"Django: {django.__version__}"),
+        html.li(f"Iommi: {iommi.__version__}"),
+    )
+
+
+class ModelPages(MyPage):
+    class Meta:
+        title = "Pages"
+
+    model_pages = html.ul(
+        hyperlink("Artists", url_name="artists"),
+    )
+
+
+class ErrorPages(MyPage):
+    class Meta:
+        title = "Errors"
+
+    error_pages = html.ul(
+        hyperlink("Page parts must be dict, not list", url_name="error_page_parts_must_be_dict_not_list")
+    )
+
+
 def index(request):
-    class VersionInfo(Page):
-        class Meta:
-            title = "Versions"
-            h_tag__tag = "h3"
-
-        versions = html.ul(
-            html.li(f"Python: {platform.python_version()}"),
-            html.li(f"Django: {django.__version__}"),
-            html.li(f"Iommi: {iommi.__version__}"),
+    return Page(
+        parts=dict(
+            version_info=VersionInfo(),
+            model_pages=ModelPages(),
+            error_pages=ErrorPages(),
         )
-
-    return VersionInfo()
+    )
