4701633158442b555ea5ff18aaa2ac1d7b9a2115
Maximilian Albert <maximilian.albert@gmail.com>
2022-07-08 01:10:30 +0100

    Add explanatory paragraph to the error page example

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 import platform
+import textwrap
 
 import django
 import iommi
@@ -11,8 +12,8 @@ class MyPage(Page):
         h_tag__tag = "h4"
 
 
-def hyperlink(link_text, url_name):
-    return html.li(html.a(link_text, attrs__href=reverse_lazy(url_name)))
+def hyperlink(link_text, url_name, enclosing_tag=True):
+    return html.a(link_text, attrs__href=reverse_lazy(url_name))
 
 
 class VersionInfo(MyPage):
@@ -26,27 +27,55 @@ class VersionInfo(MyPage):
     )
 
 
-class ModelPages(MyPage):
+class DatabaseRecords(MyPage):
     class Meta:
-        title = "Pages"
+        title = "Database Records"
 
     model_pages = html.ul(
-        hyperlink("Artists", url_name="artists"),
+        html.li(hyperlink("Artists", url_name="artists")),
     )
 
 
-class ErrorPages(MyPage):
+def error_example(title, *paragraphs, url_name):
+    return html.li(
+        hyperlink(title, url_name=url_name),
+        *(
+            html.div(paragraph_text, attrs__style={"padding-top": "10px", "font-size": "smaller"})
+            for paragraph_text in paragraphs
+        )
+    )
+
+
+class ImprovableErrorMessages(MyPage):
     class Meta:
-        title = "Errors"
+        title = "Pages With Errors"
 
     intro_text = html.div(
-        html.i(
-            "Examples of pages that deliberately contain errors but where the error message could be clearer:",
-        ),
+        "The following examples deliberately produce errors. "
+        "They are meant to illustrate examples where the error message could be clearer.",
+        attrs__style={"padding-top": "5px", "padding-bottom": "15px"},
     )
 
     error_pages = html.ul(
-        hyperlink("Page parts must be dict, not list", url_name="error_page_parts_must_be_dict_not_list")
+        error_example(
+            "Page parts must be dict, not list",
+            (
+                """
+                I tried to create a page by passing a list of its parts instead of a dict, like so:
+                `Page(parts=[...])`.
+                """
+            ),
+            (
+                """
+                The current error message is:
+                `descriptor 'items' for 'dict' objects doesn't apply to a 'list' object`.
+                While this is reasonably clear, it doesn't provide any context why
+                this error occurs. It would be nice to add an explanation, possibly
+                linking to the docs page "Everything in iommi has a name".
+                """
+            ),
+            url_name="error_page_parts_must_be_dict_not_list",
+        ),
     )
 
 
@@ -54,7 +83,7 @@ def index(request):
     return Page(
         parts=dict(
             version_info=VersionInfo(),
-            model_pages=ModelPages(),
-            error_pages=ErrorPages(),
+            model_pages=DatabaseRecords(),
+            error_pages=ImprovableErrorMessages(),
         )
     )
